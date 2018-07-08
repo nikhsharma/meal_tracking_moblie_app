a4b051cabcfc14437828b60e770c3ebdaac46c5b
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-07-08 16:54:03 +0100

    Add API call to search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,16 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: 'ads'
+      search: '',
+      results: ''
     }
     this.search = this.search.bind(this);
   }
 
   search(text) {
     console.log('searching for', text);
+    fetch('https://uk.openfoodfacts.org/cgi/search.pl?search_terms=' + `${text}` + '&search_simple=1&action=process&json=1').then(res => res.json()).then(data => this.setState({results: data}))
+    console.log(this.state.results);
   }
 
   render() {
